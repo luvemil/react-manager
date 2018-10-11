bf9884f5caf0b53dfbaa735fca19c3279838eeae
Marco Tarantino <taran.marco@gmail.com>
2018-10-11 21:33:30 +0200

    Prototype form control

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,28 +2,61 @@ import React, { Component } from 'react';
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
 import Container from 'react-bootstrap/lib/Container';
+import Form from 'react-bootstrap/lib/Form';
+import InputGroup from 'react-bootstrap/lib/InputGroup';
+import Button from 'react-bootstrap/lib/Button';
 import './Portfolio.css';
 
-
 function PositionRow(props) {
-  let cols, data_a;
+  let cols;
   if (props.is_header) {
-    cols = <PositionCols name="header" data_a={props.display_var} />;
+    cols = props.display_vars.map((x,i) => <Col key={`col-header-${i}`}>{x}</Col>);
   } else {
-    data_a = props.display_var.map((x) => {
-      if (x == 'value') {
+    let data_a = props.display_vars.map((x) => {
+      if (x === 'value') {
         return props.data['price'] * props.data['qty'];
       } else {
         return props.data[x];
       }
     });
-    cols = <PositionCols name={props.data.name} data_a={data_a} />;
+    cols = data_a.map((x,i) => <Col key={`col-${props.name}-${i}`}>{x}</Col>);
   }
 
   return (
     <Row>
       { cols }
     </Row>
+  );
+}
+
+function AddPositionRow(props) {
+  // Check
+  // - https://stackoverflow.com/questions/38511906/handle-change-of-formcontrol-react
+  // - https://itnext.io/easy-form-handling-in-react-be5490a1284
+  // - https://blog.logrocket.com/an-imperative-guide-to-forms-in-react-927d9670170a
+  // to see how to control form values.
+  let cols = props.display_vars.map((x,i) => {
+    if ( i === props.display_vars.length - 1 ) {
+      return (
+        <Col key={`${i}`}>
+          <InputGroup>
+            <Form.Control />
+            <InputGroup.Append>
+              <Button variant="primary" type="submit">+</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Col>
+      )
+    } else {
+      return <Col key={i} name={x} ><Form.Control /></Col>
+    }
+  });
+  return (
+    <Form onSubmit={props.onSubmit}>
+      <Form.Row>
+        { cols }
+      </Form.Row>
+    </Form>
   );
 }
 
@@ -35,28 +68,39 @@ class Portfolio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      display_var: ['name','exchange','qty','price','value'],
+      display_vars: ['name','exchange','qty','price','value'],
+      user_defined_vars: ['name','exchange','qty','price'],
       data: [
         { name: "BTC", exchange: 'best crypto traders', qty: 1.4, price: 5000 },
         { name: "BTC", exchange: 'not that great', qty: 1.4, price: 5000 },
         { name: "ETH", exchange: 'not that great', qty: 12, price: 100 },
         { name: "USD", exchange: 'my pockets', qty: 2000, price: 1},
       ],
+      new_position: { name: "", exchange: "", qty: "" },
     }
   }
 
+  handleChange(e) {
+    this.setState({ event.target.name: event.target.value });
+  }
+
+  onSubmit(e) {
+    alert(this.state.new_position.name);
+  }
+
   render() {
-    const rows = this.state.data.map((x,i) => <PositionRow key={`row-${i}`} display_var={this.state.display_var} data={x} />);
+    const rows = this.state.data.map((x,i) => <PositionRow key={`row-${i}`} display_vars={this.state.display_vars} data={x} />);
     return (
       <div className="Portfolio">
         <Container>
-          <PositionRow is_header={true} display_var={this.state.display_var} />
+          <PositionRow is_header={true} display_vars={this.state.display_vars} />
           { rows }
+          <AddPositionRow display_vars={this.state.display_vars} onSubmit={(e) => this.onSubmit(e)} onChange={event => this.handleChange(event)} />
         </Container>
       </div>
     );
   }
 }
 
-export { PositionCols, PositionRow };
+export { PositionCols, PositionRow, AddPositionRow };
 export default Portfolio;
--- src/components/Portfolio.test.js
+++ src/components/Portfolio.test.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { PositionCols, PositionRow } from './Portfolio.js';
+import { PositionCols, PositionRow, AddPositionRow } from './Portfolio.js';
 import Portfolio from './Portfolio.js';
 
-it('renders without crashing', () => {
+it('renders PositionCols', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PositionCols name="test" data_a={["bar", 22]}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders without crashing', () => {
+it('renders PositionRow as header', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<PositionRow is_header={true} display_var={["bar", 22]}/>, div);
+  ReactDOM.render(<PositionRow is_header={true} display_vars={["bar", 22]}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders without crashing', () => {
+it('renders PositionRow as data', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<PositionRow display_var={["bar", "foo"]} data={{bar: 11, foo: 22}}/>, div);
+  ReactDOM.render(<PositionRow display_vars={["bar", "foo"]} data={{bar: 11, foo: 22}}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders AddPositionRow', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AddPositionRow display_vars={["bar", "foo"]}/>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
